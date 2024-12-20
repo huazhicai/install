18b9c1fcfba1b11c2a46c0a8453760d122f16768
caizhihua <936844218@qq.com>
2024-12-20 14:09:30 +0800

    hdsc 更改yum_path

--- base/base_install.sh
+++ base/base_install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 WORKDIR=$(cd `dirname $0`;pwd)      #脚本所在路径
 echo "脚本所在路径${WORKDIR}"
-YUM_PATH=$WORKDIR/../yumpacker
+YUM_PATH=$WORKDIR/../package/base
 PROFILE_PATH=${WORKDIR}/profile
 USERS=$(grep 'USERS' ${PROFILE_PATH}|awk -F'=' '{print $2}')
 INIT_USERS=(${USERS//,/ })
