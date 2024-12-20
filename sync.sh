e1ba4f61b76ad10e3ee93e8c7a27a709f1aac4ff
caizhihua <936844218@qq.com>
2024-12-20 14:12:45 +0800

    hdsc remove config_ssh.sh

--- sync.sh
+++ sync.sh
@@ -10,4 +10,4 @@ ssh ${target_user}@${target_ip} -t -t "mkdir -p ${WORKDIR}"
 scp -r ${WORKDIR}/rsync-3.1.2-10.el7.x86_64.rpm ${target_user}@${target_ip}:${WORKDIR}/
 rpm -ivh --force --nodeps ${WORKDIR}/rsync-3.1.2-10.el7.x86_64.rpm
 ssh ${target_user}@${target_ip} -t -t "rpm -ivh --force --nodeps ${WORKDIR}/rsync-3.1.2-10.el7.x86_64.rpm"
-rsync -avzP ${WORKDIR}/{install.sh,local_ip.sh,profile,mk_secure_dir.sh,add_to_file.sh,config_ssh.sh} ${target_user}@${target_ip}:${WORKDIR}/
+rsync -avzP ${WORKDIR}/{install.sh,local_ip.sh,profile,mk_secure_dir.sh,add_to_file.sh} ${target_user}@${target_ip}:${WORKDIR}/
