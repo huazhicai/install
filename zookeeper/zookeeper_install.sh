947a606282b4c3729fcd512726fd028a6a98682a
caizhihua <936844218@qq.com>
2024-12-21 19:01:45 +0800

    hsdc zookeeper change3

--- zookeeper/zookeeper_install.sh
+++ zookeeper/zookeeper_install.sh
@@ -12,7 +12,6 @@ INSTALL_DIR="/usr/local/zookeeper"
 # 获取所有的节点 IP
 IPS=($(grep '^member.' ${PROFILE} | awk -F'=' '{print $2}' | awk -F':' '{print $1}'))
 
-
 handle_error() {
   echo "错误: $1"
   exit 1
@@ -20,32 +19,32 @@ handle_error() {
 
 # 安装 Zookeeper
 install() {
-    # 解压 Zookeeper
-    tar -zxvf ${WORKDIR}/apache-zookeeper-${VERSION}-bin.tar.gz || handle_error "解压失败"
-    mv ${WORKDIR}/apache-zookeeper-${VERSION}-bin ${INSTALL_DIR}
+  tar -zxvf ${WORKDIR}/apache-zookeeper-${VERSION}-bin.tar.gz || handle_error "解压失败"
 
-    # 复制默认配置文件
-    cp ${INSTALL_DIR}/conf/zoo_sample.cfg ${INSTALL_DIR}/conf/zoo.cfg
+  # 移动解压后的文件到指定目录
+  mv ${WORKDIR}/apache-zookeeper-${VERSION}-bin ${INSTALL_DIR}
 
-    # 创建数据目录和日志目录
-    mkdir -p ${INSTALL_DIR}/{data,logs}
+  # 复制默认配置文件
+  cp ${INSTALL_DIR}/conf/zoo_sample.cfg ${INSTALL_DIR}/conf/zoo.cfg
 
-    # 修改配置文件
-    sed -i "s|dataDir=.*|dataDir=${INSTALL_DIR}/data|" ${INSTALL_DIR}/conf/zoo.cfg
-    sed -i "s|dataLogDir=.*|dataLogDir=${INSTALL_DIR}/logs|" ${INSTALL_DIR}/conf/zoo.cfg
+  # 创建数据目录和日志目录
+  mkdir -p ${INSTALL_DIR}/{data,logs}
 
-    # 为每个节点添加 server 配置
-    for ((i = 0; i < ${#IPS[@]}; i++))
-    do
-        # 添加 server 配置，格式为 server.x=ip:2888:3888
-        sed -i "\$a server=$((i + 1))=${IPS[i]}:2888:3888" ${INSTALL_DIR}/conf/zoo.cfg
-    done
+  # 修改配置文件
+  sed -i "s|dataDir=.*|dataDir=${INSTALL_DIR}/data|" ${INSTALL_DIR}/conf/zoo.cfg
+  sed -i "s|dataLogDir=.*|dataLogDir=${INSTALL_DIR}/logs|" ${INSTALL_DIR}/conf/zoo.cfg
 
-    # 设置 myid 文件
-    echo ${id} > ${INSTALL_DIR}/data/myid
+  # 为每个节点添加 server 配置
+  for ((i = 0; i < ${#IPS[@]}; i++)); do
+    # 添加 server 配置，格式为 server.x=ip:2888:3888
+    sed -i "\$a server=$((i + 1))=${IPS[i]}:2888:3888" ${INSTALL_DIR}/conf/zoo.cfg
+  done
 
-    # 创建 systemd 服务
-    cat <<EOF | sudo tee /etc/systemd/system/zookeeper.service
+  # 设置 myid 文件
+  echo ${id} > ${INSTALL_DIR}/data/myid
+
+  # 创建 systemd 服务
+  cat <<EOF | sudo tee /etc/systemd/system/zookeeper.service
 [Unit]
 Description=ZooKeeper Service
 After=network-online.target
@@ -62,10 +61,10 @@ WorkingDirectory=${INSTALL_DIR}
 WantedBy=multi-user.target
 EOF
 
-    # 重新加载 systemd 服务，启动 Zookeeper 服务，并设置开机自启动
-    systemctl daemon-reload
-    systemctl start zookeeper
-    systemctl enable zookeeper
+  # 重新加载 systemd 服务，启动 Zookeeper 服务，并设置开机自启动
+  systemctl daemon-reload
+  systemctl start zookeeper
+  systemctl enable zookeeper
 }
 
 # 调用安装函数
